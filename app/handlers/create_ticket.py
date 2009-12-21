2dc20b8997324dab80814c74ba9f3d62d8ac4403
doki_pen <doki_pen@doki-pen.org>
2009-12-20 19:03:52 -0500

    add uid email param

--- app/handlers/create_ticket.py
+++ app/handlers/create_ticket.py
@@ -2,14 +2,18 @@ import logging
 from lamson.routing import route, route_like, stateless
 from config.settings import relay, TRAC_URL
 from lamson import view
-from email import Utils
 from xmlrpclib import ServerProxy
 
 
-@route("(project)@(host)", project=".+", host=".+")
-def START(message, project=None, host=None):
+@route("(project)[+](resource_uid)@(host)", project="[^+]+", resource_uid=".+", host=".+")
+@route("(project)@(host)", project="[^+]+", host=".+")
+def START(message, project=None, resource_uid=None, host=None):
     tracserver = ServerProxy(TRAC_URL)
-    ticketid = tracserver.tracgate.recieve(message.body(), message.base.headers)
+    ticketid = tracserver.tracgate.recieve(
+        message.body(), 
+        message.base.headers,
+        resource_uid or ''
+    )
     return START
 
 
