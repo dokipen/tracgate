05ccd30cc98e4c076ef29f6589fb6f19f6e97c24
doki_pen <doki_pen@doki-pen.org>
2009-12-28 08:03:09 -0500

    added try/except to xmlrpc call

--- app/handlers/trac_gate.py
+++ app/handlers/trac_gate.py
@@ -3,16 +3,19 @@ from lamson.routing import route, route_like, stateless
 from config.settings import trac_resolver
 from lamson import view
 
-
 @route("(project)[+](resource_uid)@(host)", project="[^+]+", resource_uid=".+", host=".+")
 @route("(project)@(host)", project="[^+]+", host=".+")
 def START(message, project=None, resource_uid=None, host=None):
     tracserver = trac_resolver.resolve(project)
-    return_val = tracserver.tracgate.recieve(
-        message.body(), 
-        message.base.headers,
-        resource_uid
-    )
+    try:
+        return_val = tracserver.tracgate.recieve(
+            message.body(), 
+            message.base.headers,
+            resource_uid
+        )
+    except:
+        logging.exception("XML-RPC call failed")
+
     return START
 
 
